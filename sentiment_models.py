1dbb14725356f45a3cd94490ec1ecdb9202518cd
shoarora <shoarora@stanford.edu>
2017-07-05 04:58:59 -0700

    squeeze dims

--- sentiment_models.py
+++ sentiment_models.py
@@ -112,7 +112,7 @@ class QRNNModel(SentimentModel):
         lens = self.lens
         final_states = []
         for i, point in enumerate(tf.unstack(x, axis=0)):
-            print i, lens[i]
+            print i, lens, lens[i]
             print point
             final_states.append(tf.squeeze(point[lens[i]-1, :]))
         return tf.stack(x, axis=0), weights
